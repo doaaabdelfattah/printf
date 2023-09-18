9268ead29204a5b0881fa4bd808dbb7b9daaa194
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-09-18 16:23:12 -0700

    printF files

--- _printf.c
+++ _printf.c
@@ -1,3 +1,50 @@
 #include "main.h"
 
+/**
+*_printf - print output
+*@format: pointer to text
+*Return: int
+*/
+int _printf(const char *format, ...)
+{
+int i, count;
+int (*operation)(va_list);
+va_list arglist;
+
+va_start(arglist, format);
+i = 0;
+if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
+{
+return (-1);
+}
+else
+{
+count = 0;
+for (; format && format[i] != '\0' ; i++)
+{
+if (format[i] != '%')
+{
+output_1(format[i]);
+count++;
+}
+else
+{
+{
+i++;
+operation = get_func_print(format[i]);
+if (operation)
+count += operation(arglist);
+else if (operation == NULL)
+{
+count++;
+output_1('%');
+}
+}
+}
+}
+}
+va_end(arglist);
+return (count);
+}
+
 
--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "main.h"
-
+#include <stdio.h>
 int main(void)
 {
-	_printf(" first letter is %c My name is %s \n", 'd', "doaa");
+_printf(" first letter is %c My name is %s \n", 'd', "doaa");
+
+_printf("%s%c%c%c%s%%%s%c\n", 
+"Loading", '.', '.', '.', "99", " Please wait", '\n');
+printf("%s%c%c%c%s%%%s%c\n", 
+"Loading", '.', '.', '.', "99", " Please wait", '\n');
+_printf("css%ccs%scscses\n", 'T', "Test");
+
+return (0);
 }
