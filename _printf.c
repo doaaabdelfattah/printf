782524779139617440ef8d50179c0e8ebfdcf74a
hanaaattya <hanaaattya6565@gmail.com>
2023-09-19 15:09:59 +0300

    commit

--- _printf.c
+++ _printf.c
@@ -5,45 +5,49 @@
 *@format: pointer to text
 *Return: int
 */
-int _printf(const char *format, ...)
+	int _printf(const char *format, ...)
 {
-int i, count;
-int (*operation)(va_list);
-va_list arglist;
+	int i, count;
+	int (*operation)(va_list);
+	va_list arglist;
 
-va_start(arglist, format);
-i = 0;
-if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
+	va_start(arglist, format);
+	i = 0;
+	if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
 {
-return (-1);
+	return (-1);
 }
-else
+	else
 {
-count = 0;
-for (; format && format[i] != '\0' ; i++)
+	count = 0;
+	for (; format && format[i] != '\0' ; i++)
 {
-if (format[i] != '%')
+	if (format[i] != '%')
 {
-output_1(format[i]);
-count++;
+	output_1(format[i]);
+	count++;
+	}
+	else
+	{
+	{
+	i++;
+	if (format[i] == '%')
+	count += my_p_perc('%');
+	else
+{
+	operation = get_func_print(format[i]);
+	if (operation)
+	count += operation(arglist);
+	else if (operation == NULL)
+{
+	count++;
+	output_1('%');
 }
-else
-{
-{
-i++;
-if (format[i] == '%')
-count += my_p_perc('%');
-else
-{
-operation = get_func_print(format[i]);
-if (operation)
-count += operation(arglist);
-else if (operation == NULL)
-{
-count++;
-output_1('%');
-}}}}}
 }
-va_end(arglist);
-return (count);
+}
+}
+}
+}
+	va_end(arglist);
+	return (count);
 }
